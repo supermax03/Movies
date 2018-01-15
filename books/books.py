0fd95faa6afefc2e9d7b285ddef2ebd060b2835a
supermax03 <maximiliano.bordon@gmail.com>
2018-01-15 01:04:32 -0300

    add books.py

--- books/books.py
+++ books/books.py
@@ -1,9 +1,11 @@
-#TODO: Add Google book api
 import requests
 
+
 class GoogleBook:
-     __url__="https://www.googleapis.com/books/v1/volumes?q={0}"
-     @staticmethod
-     def getbooks(term):
-                 page=requests.get(GoogleBook.__url__.format(term))
-                 return page.content
+    __url__ = "https://www.googleapis.com/books/v1/volumes?q={0}"
+
+    @staticmethod
+    def getbooks(term):
+        term = "+".join(term.split(" "))
+        page = requests.get(GoogleBook.__url__.format(term))
+        return page.content
